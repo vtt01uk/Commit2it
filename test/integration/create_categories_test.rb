9f2502f5e81b6e7e84435a115da5456a721601b8
vtt01uk <vtt01uk@gmail.com>
2016-02-01 11:00:15 +0000

    Edited images

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -14,5 +14,17 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 		#Ensure the word 'sports' exists in the page
 		assert_match "sports", response.body
 	end
+	
+	test "invalid category submission results in failure" do
+		get new_category_path
+		assert_template 'categories/new'
+		assert_no_difference 'Category.count'do
+			post categories_path, category: {name: " "}
+		end
+		assert_template 'categories/new'
+		assert_select 'h2.panel-title' #from _errors.html.erb file
+		assert_select 'div.panel-body' #from _errors.html.erb file
+	end
+	
 end
 
